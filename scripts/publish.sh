ef752f3eae851152823f288cdadf70ddea9aae6f
John L. Preston <wcerfgba@riseup.net>
2017-10-21 19:46:20 +0100

    Fix publish script for gh-pages

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-git checkout master         &&
-yarn install                &&
-yarn build                  &&
-mv build .build             &&
-git checkout gh-pages       &&
-rm -rf *                    &&
-mv .build/* .               &&
-rm -r .build                &&
-git add .                   &&
-git commit -m `date -Ins`   &&
+cat > preact.config.js <<EOF
+export default function (config, env, helpers) {
+	config.output.publicPath = '/easysampler/';
+  config.module.loaders[0].options.name = (filename) => {
+    let relative = filename.replace(src, config.output.publicPath);
+    let isRoute = filename.indexOf('/routes/') >= 0;
+
+    return isRoute ? 'route-' + relative.replace(/(^\/(routes|components\/(routes|async))\/|(\/index)?\.js$)/g, '') : false;
+  };
+}
+EOF
+yarn install                                        &&
+yarn build                                          &&
+mv build .build                                     &&
+git checkout gh-pages                               &&
+rm -rf *                                            &&
+mv .build/* .                                       &&
+rm -r .build                                        &&
+git add .                                           &&
+git commit -m `date -Ins`                           &&
 git checkout master
