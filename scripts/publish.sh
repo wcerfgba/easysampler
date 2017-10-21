534c597c1fbce05769536b6b3ee46ea4fdbd4ac5
John L. Preston <wcerfgba@riseup.net>
2017-10-21 19:26:55 +0100

    Add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git checkout master         &&
+yarn install                &&
+yarn build                  &&
+mv build .build             &&
+git checkout gh-pages       &&
+rm -rf *                    &&
+mv .build/* .               &&
+rm -r .build                &&
+git add .                   &&
+git commit -m `date -Ins`   &&
+git checkout master
