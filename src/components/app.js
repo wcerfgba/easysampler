80c88ed40c75262eaa7c772a9927ec659fe16cbc
John L. Preston <wcerfgba@riseup.net>
2017-10-22 12:10:34 +0100

    Eject from preact-cli

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
+import { createBrowserHistory } from 'history';
 
 import Header from './header';
 import Home from '../routes/home';
@@ -9,10 +10,20 @@ export default class App extends Component {
 		this.currentUrl = e.url;
 	};
 
+	constructor(props) {
+		super(props);
+		this.history = createBrowserHistory({
+			basename: '###BASENAME###'
+		});
+	}
+
 	render() {
 		return (
 			<div id="app">
-				<Router onChange={this.handleRoute}>
+				<Router
+					onChange={this.handleRoute}
+					history={this.history}
+				>
 					<Home path="/" />
 				</Router>
 			</div>
