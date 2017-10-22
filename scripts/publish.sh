f55f9c62cceacc92d7dc454c0a4b75f395647617
John L. Preston <wcerfgba@riseup.net>
2017-10-22 10:47:41 +0100

    Still trying to get deploy working...

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-cat > preact.config.js <<EOF
-export default function (config, env, helpers) {
-	config.output.publicPath = '/easysampler/';
-  config.module.loaders[0].options.name = (filename) => {
-    let relative = filename.replace(src, config.output.publicPath);
-    let isRoute = filename.indexOf('/routes/') >= 0;
-
-    return isRoute ? 'route-' + relative.replace(/(^\/(routes|components\/(routes|async))\/|(\/index)?\.js$)/g, '') : false;
-  };
-}
-EOF
 yarn install                                        &&
 yarn build                                          &&
 mv build .build                                     &&
